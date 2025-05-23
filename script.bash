72ddf105c34c724ea3a34a1b6c9e403311420fb8
VardhanLearn <vardhanlearner@gmail.com>
2025-05-23 07:50:08 +0530

    My first commit

---
+++ script.bash
@@ -0,0 +1,4 @@
+#Print 1 to 10
+for I in {1..10}; do
+    echo $I
+done
---
+++ script.sh
@@ -0,0 +1,9 @@
+#Print 1 to 10
+for I in {1..10}; do
+    echo $I
+    sleep 1
+done
+
+#Version control system
+1. Saving your code.
+2. Tracking changes
